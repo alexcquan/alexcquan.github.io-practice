8da7dd9ff94ef397cfc23acfcf6e2080cf4f2a5d
alexcquan <49961935+alexcquan@users.noreply.github.com>
2019-10-29 15:42:13 -0700

    JS Updated

--- Class Exercise/JQuery Part 2/01_Conditionals/js/index.js
+++ Class Exercise/JQuery Part 2/01_Conditionals/js/index.js
@@ -1,4 +1,4 @@
-$('.tab').on("click", function(){
+$('.tab').on("click", function () {
   if ($(this).hasClass("tab1")) {
     $(".tab1").addClass("active");
     $(".tab2").removeClass("active");
@@ -9,8 +9,27 @@ $('.tab').on("click", function(){
   else if ($(this).hasClass("tab2")) {
     $(".tab2").addClass("active");
     $(".tab1").removeClass("active");
-    $("#tabContent2").css("display", "block");
-    $("#tabContent1").css("display", "none");
+    $("#tabContent1").css("display", "block");
+    $("#tabContent2").css("display", "none");
   }
 
+});
+$('.tab').on("click", function () {
+  if ($(this).hasClass("tab1")) {
+    $(".tab1").addClass("active");
+    $(".tab2").removeClass("active");
+    $("#tabContent1").css("color", "blue");
+    $("#tabContent1").css("display", "block");
+    $("#tabContent2").css("color", "blue");
+    $("#tabContent2").css("display", "none");
+  }
+
+  else if ($(this).hasClass("tab2")) {
+    $(".tab1").removeClass("active");
+    $(".tab2").addClass("active");
+    $("#tabContent1").css("color", "red");
+    $("#tabContent1").css("display", "none");
+    $("#tabContent2").css("color", "red");
+    $("#tabContent2").css("display", "block");
+  }
 });
--- Class Exercise/JQuery Part 2/03_todo_app/js/index.js
+++ Class Exercise/JQuery Part 2/03_todo_app/js/index.js
@@ -1,5 +1,30 @@
 // Create event listener to enter items below
+$("#enter").click(function () {
+    var task = $("#todoItem").val();
+    var myArray =['cat', 'dog'];
+    //Playing Around
+    if (task == '' || task == 'undefined') {
+        // alert("Please enter a task");
+        myArray.push('Yay my array worked');
+        console.log(myArray);
+    }
+    else {
+        $("#todoList").append("<div class='task'>" + task + "<div class='x fas fa-times'></div></div>");
+    }
+});
 
 // Create event listener to delete items below
-
+$(document).on("click", ".x", function () {
+    $(this).parent().remove();
+});
 // Create event listener to toggle 
+$(document).on("click", ".task", function () {
+    $(this).toggleClass("done");
+    if ($(this).hasClass("done")) {
+        $(this).find("div").removeClass("fa-times");
+        $(this).find("div").addClass("fa-check");
+    }
+    else {
+        $(this).find("div").addClass("fa-times");
+    }
+});
